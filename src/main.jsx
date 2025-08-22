eb3b3daf06eb06f221ebcdc2bc0e8293dd6eb63d
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-08-22 16:35:48 +0800

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import '../dist/scss/main.css';
+import '../scss/main.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
